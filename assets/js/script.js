04006f26b1419391eb71c1e5aaef493f0abe2500
joaquinmorassut <joaco_nemesis@hotmail.com>
2023-02-20 04:51:03 -0300

    agregue readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-// Variables
+alert("Bienvenido a la pagina de electricidad e iluminacion Fosam");
+
 let nombreCliente = prompt("Ingrese su nombre:");
+console.log(`Bienvenido a la tienda de electricidad/iluminación, ${nombreCliente}!`);
+
+let nombreApellido = prompt("Ingrese su apellido:");
+console.log(`Bienvenido a la tienda de electricidad/iluminación, ${nombreApellido}!`);
+
+alert("a continuacion le solicitaremos la cantidad de productos que desee llevar");
+
 let productos = ["bombillas", "cables", "enchufes"];
+console.log(`Los productos disponibles son: ${productos}`);
+
 let precioProductos = [2.99, 4.99, 1.99];
+console.log(`Los precios de los productos son: ${precioProductos}`);
+
 let cantidadProductos = [];
+console.log(`La cantidad de cada producto que has elegido es: ${cantidadProductos}`);
+
 let descuento = 0.1;
 
 // Algoritmos
+let subtotal = 0;
 for (let i = 0; i < productos.length; i++) {
     let cantidad = parseInt(prompt(`Ingrese la cantidad de ${productos[i]}:`));
     cantidadProductos.push(cantidad);
-}
-let subtotal = 0;
-for (let i = 0; i < productos.length; i++) {
     subtotal += precioProductos[i] * cantidadProductos[i];
 }
+console.log(`El subtotal es: ${subtotal.toFixed(2)}`);
 
 // anidaciones
-
 let total;
 if (subtotal > 50 && subtotal <= 100) {
     total = subtotal * (1 - descuento);
@@ -25,28 +37,26 @@ if (subtotal > 50 && subtotal <= 100) {
 } else {
     total = subtotal;
 }
-
 let totalString = total.toFixed(2);
 let totalNum = parseFloat(totalString);
 
-// Script
-console.log(`Bienvenido a la tienda de electricidad/iluminación, ${nombreCliente}!`);
-console.log(`Los productos disponibles son: ${productos}`);
-console.log(`Los precios de los productos son: ${precioProductos}`);
-console.log(`La cantidad de cada producto que has elegido es: ${cantidadProductos}`);
-console.log(`El subtotal es: ${subtotal.toFixed(2)}`);
-console.log(`El total con descuento es: ${totalNum.toFixed(2)}`);
-
 // Condicionales y estructura if
-if (total > 50) {
+let envioGratuito = false;
+if (total >= 50) {
     console.log("¡Felicidades! ¡Tienes derecho a envío gratuito!");
+    alert("¡Felicidades! ¡Tienes derecho a envío gratuito!");
+    envioGratuito = true;
 } else {
     console.log("Lamentablemente, no cumples con el monto mínimo para envío gratuito.");
+    alert("Lamentablemente, no cumples con el monto mínimo para envío gratuito.");
 }
 
+console.log(`Gracias, ${nombreCliente}! Su compra total fue de ${totalNum.toFixed(2)} y hemos enviado un recibo a su dirección de correo electrónico.`);
 // Scope
 function imprimirMensaje() {
     let mensaje = "Gracias por tu compra!";
     console.log(mensaje);
 }
 imprimirMensaje();
+
+
